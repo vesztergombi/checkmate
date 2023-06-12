74ef9851232b25114ad016946336099b39e3a7fc
vesztergombi <vesztergombi.gyorgy@gmail.com>
2023-06-12 21:29:53 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # checkmate
-Neovim plugin for creating and toggling checkboxes
+
+Neovim plugin for creating and toggling checkboxes.
+
+# API
+
+The API is very simple and exposes two commands.
+```
+CheckmateCreateCheckBox
+CheckmateToggleCheckBox
+```
+
+Default mappings
+
+**mo** for CheckmateCreateCheckBox:
+Inserts a line below like 'o', but with a checkbox and a bullet point '*'.
+* [ ]
+
+**mx** for CheckmateToggleCheckBox.
+Replaces [ ] --> [X] or vice versa.
---
+++ lua/checkmate.lua
@@ -0,0 +1,20 @@
+M = {}
+
+M.toggle = function()
+    local lineNum = vim.api.nvim_win_get_cursor(0)
+    local row, col = unpack(lineNum)
+    print('row', row, 'col', col)
+
+    local line = vim.api.nvim_get_current_line()
+    local test = "* [ ] bla bla [X]"
+    local repl = {}
+    repl[' '] = '[X]'
+    repl['X'] = '[ ]'
+    local result = string.gsub(line, '%[([ X])%]', repl, 1)
+    vim.api.nvim_buf_set_lines(0, row-1, row, true, {result})
+    print(result)
+end
+
+M.toggle()
+
+return M
